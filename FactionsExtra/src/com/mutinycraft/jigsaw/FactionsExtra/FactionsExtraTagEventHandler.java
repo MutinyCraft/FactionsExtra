2843f0e5f79bb7a17f15dead696307d6b2e2c6d2
MutinyCraft <admin@mutinycraft.com>
2014-01-26 10:54:50 -0600

    Update to use TagAPI's Async event.

--- FactionsExtra/src/com/mutinycraft/jigsaw/FactionsExtra/FactionsExtraTagEventHandler.java
+++ FactionsExtra/src/com/mutinycraft/jigsaw/FactionsExtra/FactionsExtraTagEventHandler.java
@@ -3,7 +3,7 @@ package com.mutinycraft.jigsaw.FactionsExtra;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.kitteh.tag.PlayerReceiveNameTagEvent;
+import org.kitteh.tag.AsyncPlayerReceiveNameTagEvent;
 
 import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.FPlayers;
@@ -20,19 +20,21 @@ public class FactionsExtraTagEventHandler implements Listener {
 	// Tag Event
 
 	@EventHandler
-	public void factionTag(PlayerReceiveNameTagEvent event) {
-		FPlayer namedPlayer = FPlayers.i.get(event.getNamedPlayer());
-		FPlayer seeingPlayer = FPlayers.i.get(event.getPlayer());
+	public void factionTag(AsyncPlayerReceiveNameTagEvent event) {
+        FPlayer namedPlayer = FPlayers.i.get(event.getNamedPlayer());
+        FPlayer seeingPlayer = FPlayers.i.get(event.getPlayer());
 
-		if (namedPlayer.getFaction().isPeaceful()) {
-			event.setTag(ChatColor.GOLD + event.getNamedPlayer().getName());
-		} else if (namedPlayer.getRelationTo(seeingPlayer).isEnemy()) {
-			event.setTag(ChatColor.RED + event.getNamedPlayer().getName());
-		} else if (namedPlayer.getRelationTo(seeingPlayer).isAlly()) {
-			event.setTag(ChatColor.DARK_GREEN
-					+ event.getNamedPlayer().getName());
-		} else if (namedPlayer.getRelationTo(seeingPlayer).isMember()) {
-			event.setTag(ChatColor.GREEN + event.getNamedPlayer().getName());
-		}
+        if(namedPlayer.getName().length() >= 14){
+            // Do nothing
+        } else if (namedPlayer.getFaction().isPeaceful()) {
+            event.setTag(ChatColor.GOLD + event.getNamedPlayer().getName());
+        } else if (namedPlayer.getRelationTo(seeingPlayer).isEnemy()) {
+            event.setTag(ChatColor.RED + event.getNamedPlayer().getName());
+        } else if (namedPlayer.getRelationTo(seeingPlayer).isAlly()) {
+            event.setTag(ChatColor.DARK_GREEN
+                    + event.getNamedPlayer().getName());
+        } else if (namedPlayer.getRelationTo(seeingPlayer).isMember()) {
+            event.setTag(ChatColor.GREEN + event.getNamedPlayer().getName());
+        }
 	}
 }
